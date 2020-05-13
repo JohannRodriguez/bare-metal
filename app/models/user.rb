6bc438972bd5a9ebb7b894bd06ee111e8587267c
Johann <themrblacksapce@hotmail.com>
2020-05-13 11:01:13 -0600

    rubocop errors corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# rubocop:disable Style/HashSyntax
 class User < ApplicationRecord
   validates :username, :email, presence: true
   validates :email, :username, uniqueness: true
   validates :password, presence: true, :on => :create
 end
+# rubocop:enable Style/HashSyntax
