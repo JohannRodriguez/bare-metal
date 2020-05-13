f180d618615b088eeb2a6d0a7b1395d19a6339c7
Johann <themrblacksapce@hotmail.com>
2020-05-13 10:52:16 -0600

    change validations for password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,11 +18,11 @@ class UsersController < ApplicationController
   end
 
   def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        redirect_to edit_user_path(@user)
-      else
-        render :edit
-      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, :email, presence: true
   validates :email, :username, uniqueness: true
+  validates :password, presence: true, :on => :create
 end
